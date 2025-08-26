d0fc9096d76e25860b75155033e9ecc1261f7c67
Leo5300 <lleonardoou89325@gmail.com>
2025-08-25 21:35:20 -0300

    NEW: criacao tabuleiro 3x3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 // Criação do elemento Square que irá compor o tabuleiro
 import '/App.css';
 
-export default function Square(){
-return{
-  <button className="Square"></button>
-  }
+function Square({valor}){
+  return(
+  <button className='square'>valor</button>
+  );
+}
+
+export default function tabuleiro() {
+  return(
+  <div>
+    <div>
+    <Square valor="1"></Square>
+   <Square valor="2"></Square>
+  <Square valor="3"></Square>
+</div>
+  <div>
+
+ <Square valor="4"></Square>
+  <Square valor="5"></Square>
+   <Square valor="6"></Square>
+</div>
+<div>
+   <Square valor="7"></Square>
+  <Square valor="8"></Square>
+  <Square valor="9"></Square>
+ </div>
+ </div>
+)
+
 
 }
